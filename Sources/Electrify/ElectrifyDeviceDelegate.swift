94efacca4955d33afe1d39a91840ab1fe04505dc
Brett Best <brettbest@live.com.au>
2019-05-22 05:02:26 +1000

    Updated logging levels

--- Sources/Electrify/ElectrifyDeviceDelegate.swift
+++ Sources/Electrify/ElectrifyDeviceDelegate.swift
@@ -12,11 +12,11 @@ class ElectrifyDeviceDelegate: DeviceDelegate {
   weak var thermostat: Service.Thermostat?
   
   func didRequestIdentificationOf(_ accessory: Accessory) {
-    logger.info("Requested identification of accessory \(String(describing: accessory.info.name.value ?? ""))")
+    logger.verbose("Requested identification of accessory \(String(describing: accessory.info.name.value ?? ""))")
   }
   
   func characteristic<T>(_ characteristic: GenericCharacteristic<T>, ofService service: Service, ofAccessory accessory: Accessory, didChangeValue newValue: T?) {
-    logger.info("Characteristic \(characteristic) in service \(service.type) of accessory \(accessory.info.name.value ?? "") did change: \(String(describing: newValue))")
+    logger.verbose("Characteristic \(characteristic) in service \(service.type) of accessory \(accessory.info.name.value ?? "") did change: \(String(describing: newValue))")
     
     if let thermostat = thermostat {
       if service != thermostat {
@@ -40,11 +40,11 @@ class ElectrifyDeviceDelegate: DeviceDelegate {
   }
   
   func characteristicListenerDidSubscribe(_ accessory: Accessory, service: Service, characteristic: AnyCharacteristic) {
-    logger.info("Characteristic \(characteristic) in service \(service.type) of accessory \(accessory.info.name.value ?? "") got a subscriber")
+    logger.verbose("Characteristic \(characteristic) in service \(service.type) of accessory \(accessory.info.name.value ?? "") got a subscriber")
   }
   
   func characteristicListenerDidUnsubscribe(_ accessory: Accessory, service: Service, characteristic: AnyCharacteristic) {
-    logger.info("Characteristic \(characteristic) in service \(service.type) of accessory \(accessory.info.name.value ?? "") lost a subscriber")
+    logger.verbose("Characteristic \(characteristic) in service \(service.type) of accessory \(accessory.info.name.value ?? "") lost a subscriber")
   }
   
 }
